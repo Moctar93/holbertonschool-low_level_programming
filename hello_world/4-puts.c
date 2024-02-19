840ce66685a40413727147d9c05ea19794118309
Moctar93 <moctartize93@gmail.com>
2024-02-19 07:05:39 -0800

    Helle, World Project

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #!/bin/bash
 int main();
 {
-char Mychar[]= "Programming is like building a multilingual puzzle";
-puts(Mystr);
+puts("Programming is like building a multilingual puzzle");
 return 0;
 }
