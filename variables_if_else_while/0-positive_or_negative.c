8605a4c07ce8805344a3cf6ee43c5f7c3437fdc8
Moctar93 <moctartize93@gmail.com>
2024-02-20 04:43:18 -0800

    condition 0-positif or negatif

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*This program will assign a random number to the variable n
+
+/**
+ *main - Prints a random number and states whether
+ * it is positive, negetive, or zero
  *
- * and print whether the number stored in the variable n is positive or negative
+ * Return: Always 0
  */
 int main(void)
 {
        	int n;
-	stand(time(0));
+	
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if n < 0
+	if (n > 1)
 		printf("%d is positive\n", n);
-	else if n == 0
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
 		printf("%d is zero\n", n);
-	else n > 0 
-		printf("%d The number is negative\n", n);
 
 	return (0);
 }
