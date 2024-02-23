bf8bbb970c04de8d74f85fba6958e75bd5459152
Moctar93 <moctartize93@gmail.com>
2024-02-23 12:34:29 +0000

    function that prints the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet-print alphabets
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
