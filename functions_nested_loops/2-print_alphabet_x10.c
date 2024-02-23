ab047050ae46a5704a1f1eefb9de613120f520c1
Moctar93 <moctartize93@gmail.com>
2024-02-23 13:13:29 +0000

    function that prints the alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints 10 times the alphabet
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int c;
+	
+	for(c = 'a'; c <= 'z'; ++c)
+		
+	_putchar(c);
+	_putchar('\n');
+
+}
