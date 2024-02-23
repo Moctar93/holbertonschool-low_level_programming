6e1d22e0eb1836291a014dc173046c851c390d3f
Moctar93 <moctartize93@gmail.com>
2024-02-23 14:31:22 +0000

    function that prints the alphabet

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_islower - function that checks for lowercase character
+ *@c: the character to check
+ * Return: 1 if c is lowercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
