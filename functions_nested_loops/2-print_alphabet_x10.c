ecb9971f1c167b8b3f66d558700ddd51825c52c4
Moctar93 <moctartize93@gmail.com>
2024-02-23 13:48:25 +0000

    function that prints the alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,12 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for(i = 0; i < 10; i++) 
+	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j < 26; j++)
+		for (j = 0; j < 26; j++)
 		{
-	_putchar('a' + j);
+		_putchar('a' + j);
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
-
 }
