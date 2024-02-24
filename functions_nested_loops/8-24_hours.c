266b2b69e433648345c8ed5ee6c07db43e635a02
ERIC GOLF KOUATCHOU <ericgolfkouatchou@iMac-de-ERIC.local>
2024-02-24 02:11:17 +0100

    prints every minute

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,14 +10,16 @@ void jack_bauer(void)
 int i, j;
 
 
-for (i = 0; i < 24; i++)
-{
-for (j = 0; j < 60; j++)
-{
-_putchar (i);
-_putchar (':');
-_putchar (j);
-_putchar ('\n');
+		for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+	{
+	_putchar('0' + i/10);
+	_putchar('0' + i%10);
+	_putchar (':');
+	_putchar ('0' + j/10);
+	_putchar ('0' + j%10);
+	_putchar ('\n');
+	}
+	}
 }
-}
-} 
