1088fb2c0cac27ef35843ccfcf385d0a24d30d87
root <moctartize93@gmail.com>
2024-03-08 14:47:54 +0000

    C - argc, argv

---
+++ argc_argv/4-add.c
@@ -0,0 +1,55 @@
+/**
+ * main - Entry point main
+ * @argc: the number of arguments
+ * @argv: the pointer containt an array of strings
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int j = 0;
+	int sum = 0;
+	int number = 0;
+	int found = 0;
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		number = atoi(argv[i]);
+		if (number < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum = sum + number;
+	}
+	printf("%i\n", sum);
+	return (found ? 1 : 0);
+}
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
