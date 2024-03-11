0607de53270e536dfbfae60a1751e1f7cd820838
root <root@HBTN-IDF-P2.localdomain>
2024-03-11 15:04:53 +0100

    update

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,5 +1,12 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * create_array -  function that creates an array of chars
+ * Return: Null if size is 0
+ * @size:size of array
+ * @c: initialisation of array
+ */
 
 char *create_array(unsigned int size, char c)
 {
@@ -7,17 +14,17 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-    {
-	    return NULL;
-    }
-    array = malloc(size * sizeof(char));
-    
-    if (array != NULL)
-    {
-	    for (i = 0; i < size; ++i)
 	{
-		array[i] = c;
+		return (NULL);
 	}
-    }
-    return (array);
+	array = malloc(size * sizeof(char));
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size; ++i)
+		{
+			array[i] = c;
+		}
+	}
+	return (array);
 }
