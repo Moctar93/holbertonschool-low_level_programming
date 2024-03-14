4c5af1143f3a509d3102a5a960c4a081e5388f8b
root <root@HBTN-IDF-P2.localdomain>
2024-03-14 11:38:21 +0100

    C - malloc, free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,34 +1,33 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
- *str_concat - function that concatenates two strings
- *@s1: chaine de caractere
- *@s2: chaine de caractere
- *Return: chaine de caractere result
+ * str_concat - concatenate two strings using malloc
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concat string
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	size_t len_s1 = strlen(s1);
-	size_t len_s2 = strlen(s2);
-	char *result;
+	char *a;
+	int i, j, c, d;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	result = (char *)malloc(len_s1 + len_s2 + 1);
-	if (result == NULL)
-	{
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
 		return (NULL);
+	for (c = 0, d = 0; c < (i + j + 1); c++)
+	{
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
 	}
-	strcpy(result, s1);
-	strcat(result, s2);
-
-	return (result);
+	return (a);
 }
